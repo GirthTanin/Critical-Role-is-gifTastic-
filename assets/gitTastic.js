94887f68f076bdd09e7d731a7c9eada683f1806e
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-06-19 18:13:22 -0400

    I spelled it wrong

--- assets/gitTastic.js
+++ assets/gitTastic.js
@@ -1,1 +1,30 @@
 // This is not just a place holder.
+
+// This will need to be changed to something else
+$("#cat-button").on("click", function() {
+
+    // This next will also need to be tweaked.
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=criticalrole";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+
+  .then(function(response) {
+
+    var imageUrl= response.data.image_original_url;
+
+    var critRoleImage = $("<img>");
+
+    critRoleImage.attr("src", imageUrl);
+    critRoleImage.attr("alt", "critRole image");
+
+    $("#images").prepend(critRoleImage);
+
+
+
+
+  });
+});

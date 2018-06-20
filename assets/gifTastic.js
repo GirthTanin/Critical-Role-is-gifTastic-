5cb079a4401d87f886a332ff0bc84076182b74ab
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-06-19 21:43:14 -0400

    some basic structure

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -1,10 +1,38 @@
-// This is not just a place holder.
+var topics = [
+  "critical-role",
+  "dungeons", 
+  "dragons", 
+  "dice", 
+  "monster", 
+  "manual", 
+  "npc",
+  "character",
+  "DM",
+  "Scanlon",
+  "Grog",
+  "Vex",
+  "Vax",
+  "Percy",
+  "Doty",
+  "Victor",
+  "Keyleth",
+  "Pike",
+  "Tiberius",
+
+]
+
+for (i=0; i<topics.length; i++)
+// append the buttons in a manner that uses them all, 
+
+
 
 // This will need to be changed to something else
-$("#cat-button").on("click", function() {
+$("#button").on("click", function() {
+
+  var subTopic= $(this).attr("data-gif");
 
     // This next will also need to be tweaked.
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=criticalrole";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + subTopic + "api_key=dc6zaTOxFJmzC&limit=10";
 
 $.ajax({
     url: queryURL,
@@ -13,12 +41,19 @@ $.ajax({
 
 
   .then(function(response) {
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+      // line 63 of 13-button-Ajax...
+      if (results[i].rating !== "r" && results[i].rating !=="pg-13") {
+        
+      }
+    }
 
-    var imageUrl= response.data.image_original_url;
+    
 
     var critRoleImage = $("<img>");
 
-    critRoleImage.attr("src", imageUrl);
+    critRoleImage.attr("src", subTopic);
     critRoleImage.attr("alt", "critRole image");
 
     $("#images").prepend(critRoleImage);
